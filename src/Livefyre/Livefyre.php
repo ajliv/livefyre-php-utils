2f7e0e7e7508fa9514bcb4f9ce8ca6a79faaf544
David Lee <dlee@livefyre.com>
2014-03-27 15:07:44 -0700

    updating documentation

--- src/Livefyre/Livefyre.php
+++ src/Livefyre/Livefyre.php
@@ -6,16 +6,15 @@ use Livefyre\Core\Network;
  * Starting point to retrieve Network & Site objects. Each object contains methods that assist with 
  * implementation in relation to its respective object.
  */
-
-/**
- * Returns a new instance of a Livefyre Network object.
- *
- * @param string $networkName Livefyre-provided Network name, e.g., "labs.fyre.co".
- * @param string $networkKey The Livefyre-provided key/id for the website or application the collection belongs
- *            to, e.g., "303617"
- * @return a Network object
- */
 class Livefyre { 
+	/**
+	 * Returns a new instance of a Livefyre Network object.
+	 *
+	 * @param string $networkName Livefyre-provided Network name, e.g., "labs.fyre.co".
+	 * @param string $networkKey The Livefyre-provided key/id for the website or application the collection belongs
+	 *            to, e.g., "303617"
+	 * @return a Network object
+	 */
 	public static function getNetwork($networkName, $networkKey) { 
 		return new Network($networkName, $networkKey);
 	} 
