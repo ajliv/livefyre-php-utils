ee3132609793d9cbaac8a5c9ee9ef41bf0cd8a2e
David Lee <dlee@livefyre.com>
2014-11-11 18:44:55 -0800

    unignoring tests

--- tests/Livefyre/Api/PersonalizedStreamsTest.php
+++ tests/Livefyre/Api/PersonalizedStreamsTest.php
@@ -1,113 +1,113 @@
 <?php
-//namespace Livefyre;
-//
-//use Livefyre\Api\PersonalizedStream;
-//use Livefyre\Factory\CursorFactory;
-//
-//class PersonalizedStreamTest extends \PHPUnit_Framework_TestCase {
-//    private $network;
-//    private $site;
-//    private $config;
-//
-//    protected function setUp() {
-//        $this->config = new LfTest();
-//        $this->config->setPropValues("prod");
-//        $this->network = Livefyre::getNetwork($this->config->NETWORK_NAME, $this->config->NETWORK_KEY);
-//        $this->site = $this->network->getSite($this->config->SITE_ID, $this->config->SITE_KEY);
-//    }
-//
-//    public function testNetworkTopicApi() {
-//        $network = $this->network;
-//        $topic = PersonalizedStream::createOrUpdateTopic($network, "1", "UNO");
-//        $this->assertFalse($topic === null);
-//
-//        $topic = PersonalizedStream::getTopic($network, "1");
-//        $this->assertFalse($topic->getCreatedAt() === null);
-//
-//        $this->assertTrue(PersonalizedStream::deleteTopic($network, $topic));
-//
-//
-//        $topics = PersonalizedStream::createOrUpdateTopics($network, array("1", "UNO"));
-//        $this->assertFalse($topics === null);
-//
-//        $topics = PersonalizedStream::getTopics($network);
-//        $this->assertFalse(sizeof($topics) === 1);
-//
-//        PersonalizedStream::deleteTopics($network, $topics);
-//    }
-//
-//    public function testSiteTopicApi() {
-//        $site = $this->site;
-//        $topic = PersonalizedStream::createOrUpdateTopic($site, "2", "DUL");
-//        $this->assertFalse($topic === null);
-//
-//        $topic = PersonalizedStream::getTopic($site, "2");
-//        $this->assertFalse($topic->getCreatedAt() === null);
-//
-//        $this->assertTrue(PersonalizedStream::deleteTopic($site, $topic));
-//
-//
-//        $topics = PersonalizedStream::createOrUpdateTopics($site, array("2", "DUL"));
-//        $this->assertFalse($topics === null);
-//
-//        $topics = PersonalizedStream::getTopics($site);
-//        $this->assertFalse(sizeof($topics) === 1);
-//
-//        PersonalizedStream::deleteTopics($site, $topics);
-//    }
-//
-//    public function testCollectionTopicApi() {
-//        $site = $this->site;
-//        $name = "PHP PSSTREAM TEST " . time();
-//        $collection = $this->site->buildLiveCommentsCollection($name, $name, $this->config->URL);
-//        $collection->createOrUpdate();
-//
-//        PersonalizedStream::getCollectionTopics($collection);
-//
-//        $topic2 = PersonalizedStream::createOrUpdateTopic($site, "2", "DUL");
-//
-//        PersonalizedStream::addCollectionTopics($collection, array($topic2));
-//
-//        $topic1 = PersonalizedStream::createOrUpdateTopic($site, "1", "HANA");
-//
-//        PersonalizedStream::replaceCollectionTopics($collection, array($topic1));
-//
-//        $name = "PHP PSSTREAM TEST " . time();
-//        $collection2 = $this->site->buildLiveCommentsCollection($name, $name, $this->config->URL);
-//        $collection2->getData()->setTopics(array($topic1, $topic2));
-//        $collection2->createOrUpdate();
-//
-//        PersonalizedStream::removeCollectionTopics($collection, array($topic1, $topic2));
-//
-//        PersonalizedStream::deleteTopics($site, array($topic1, $topic2));
-//    }
-//
-//    public function testSubscriptions() {
-//        $network = $this->network;
-//        $userToken = $network->buildUserAuthToken($this->config->USER_ID, $this->config->USER_ID . "@" . $this->config->NETWORK_NAME, $network::DEFAULT_EXPIRES);
-//
-//        $topic1 = PersonalizedStream::createOrUpdateTopic($network, "1", "HANA");
-//        $topic2 = PersonalizedStream::createOrUpdateTopic($network, "2", "DUL");
-//
-//        PersonalizedStream::getSubscriptions($network, $this->config->USER_ID);
-//
-//        PersonalizedStream::addSubscriptions($network, $userToken, array($topic1, $topic2));
-//
-//        PersonalizedStream::replaceSubscriptions($network, $userToken, array($topic2));
-//
-//        PersonalizedStream::getSubscribers($network, $topic1);
-//
-//        PersonalizedStream::removeSubscriptions($network, $userToken, array($topic2));
-//
-//        PersonalizedStream::deleteTopic($network, $topic1);
-//    }
-//
-//    public function testTimelineStream() {
-//        $network = $this->network;
-//
-//        $cursor = CursorFactory::getPersonalStreamCursor($network, $this->config->USER_ID);
-//
-//        $data = $cursor->next();
-//        $data = $cursor->previous();
-//    }
-//}
+namespace Livefyre;
+
+use Livefyre\Api\PersonalizedStream;
+use Livefyre\Factory\CursorFactory;
+
+class PersonalizedStreamTest extends \PHPUnit_Framework_TestCase {
+    private $network;
+    private $site;
+    private $config;
+
+    protected function setUp() {
+        $this->config = new LfTest();
+        $this->config->setPropValues("prod");
+        $this->network = Livefyre::getNetwork($this->config->NETWORK_NAME, $this->config->NETWORK_KEY);
+        $this->site = $this->network->getSite($this->config->SITE_ID, $this->config->SITE_KEY);
+    }
+
+    public function testNetworkTopicApi() {
+        $network = $this->network;
+        $topic = PersonalizedStream::createOrUpdateTopic($network, "1", "UNO");
+        $this->assertFalse($topic === null);
+
+        $topic = PersonalizedStream::getTopic($network, "1");
+        $this->assertFalse($topic->getCreatedAt() === null);
+
+        $this->assertTrue(PersonalizedStream::deleteTopic($network, $topic));
+
+
+        $topics = PersonalizedStream::createOrUpdateTopics($network, array("1", "UNO"));
+        $this->assertFalse($topics === null);
+
+        $topics = PersonalizedStream::getTopics($network);
+        $this->assertFalse(sizeof($topics) === 1);
+
+        PersonalizedStream::deleteTopics($network, $topics);
+    }
+
+    public function testSiteTopicApi() {
+        $site = $this->site;
+        $topic = PersonalizedStream::createOrUpdateTopic($site, "2", "DUL");
+        $this->assertFalse($topic === null);
+
+        $topic = PersonalizedStream::getTopic($site, "2");
+        $this->assertFalse($topic->getCreatedAt() === null);
+
+        $this->assertTrue(PersonalizedStream::deleteTopic($site, $topic));
+
+
+        $topics = PersonalizedStream::createOrUpdateTopics($site, array("2", "DUL"));
+        $this->assertFalse($topics === null);
+
+        $topics = PersonalizedStream::getTopics($site);
+        $this->assertFalse(sizeof($topics) === 1);
+
+        PersonalizedStream::deleteTopics($site, $topics);
+    }
+
+    public function testCollectionTopicApi() {
+        $site = $this->site;
+        $name = "PHP PSSTREAM TEST " . time();
+        $collection = $this->site->buildLiveCommentsCollection($name, $name, $this->config->URL);
+        $collection->createOrUpdate();
+
+        PersonalizedStream::getCollectionTopics($collection);
+
+        $topic2 = PersonalizedStream::createOrUpdateTopic($site, "2", "DUL");
+
+        PersonalizedStream::addCollectionTopics($collection, array($topic2));
+
+        $topic1 = PersonalizedStream::createOrUpdateTopic($site, "1", "HANA");
+
+        PersonalizedStream::replaceCollectionTopics($collection, array($topic1));
+
+        $name = "PHP PSSTREAM TEST " . time();
+        $collection2 = $this->site->buildLiveCommentsCollection($name, $name, $this->config->URL);
+        $collection2->getData()->setTopics(array($topic1, $topic2));
+        $collection2->createOrUpdate();
+
+        PersonalizedStream::removeCollectionTopics($collection, array($topic1, $topic2));
+
+        PersonalizedStream::deleteTopics($site, array($topic1, $topic2));
+    }
+
+    public function testSubscriptions() {
+        $network = $this->network;
+        $userToken = $network->buildUserAuthToken($this->config->USER_ID, $this->config->USER_ID . "@" . $this->config->NETWORK_NAME, $network::DEFAULT_EXPIRES);
+
+        $topic1 = PersonalizedStream::createOrUpdateTopic($network, "1", "HANA");
+        $topic2 = PersonalizedStream::createOrUpdateTopic($network, "2", "DUL");
+
+        PersonalizedStream::getSubscriptions($network, $this->config->USER_ID);
+
+        PersonalizedStream::addSubscriptions($network, $userToken, array($topic1, $topic2));
+
+        PersonalizedStream::replaceSubscriptions($network, $userToken, array($topic2));
+
+        PersonalizedStream::getSubscribers($network, $topic1);
+
+        PersonalizedStream::removeSubscriptions($network, $userToken, array($topic2));
+
+        PersonalizedStream::deleteTopic($network, $topic1);
+    }
+
+    public function testTimelineStream() {
+        $network = $this->network;
+
+        $cursor = CursorFactory::getPersonalStreamCursor($network, $this->config->USER_ID);
+
+        $data = $cursor->next();
+        $data = $cursor->previous();
+    }
+}
--- tests/Livefyre/Core/CollectionTest.php
+++ tests/Livefyre/Core/CollectionTest.php
@@ -1,85 +1,85 @@
 <?php
-//
-//namespace Livefyre;
-//
-//
-//use Livefyre\Dto\Topic;
-//use Livefyre\Utils\JWT;
-//
-//class CollectionTest extends \PHPUnit_Framework_TestCase {
-//    private $config;
-//    private $site;
-//
-//    protected function setUp() {
-//        $this->config = new LfTest();
-//        $this->config->setPropValues("prod");
-//        $this->site = Livefyre::getNetwork($this->config->NETWORK_NAME, $this->config->NETWORK_KEY)->getSite($this->config->SITE_ID, $this->config->SITE_KEY);
-//    }
-//
-//    public function testApi() {
-//        $name = "PHPCreateCollection" . time();
-//
-//        $collection = $this->site->buildLiveCommentsCollection($name, $name, "http://answers.livefyre.com/PHP");
-//        $collection->createOrUpdate();
-//
-//        $id = $collection->getCollectionContent()->{"collectionSettings"}->{"collectionId"};
-//        $this->assertEquals($id, $collection->getData()->getId());
-//    }
-//
-//	/**
-//	 * @covers Livefyre\Core\Collection::buildCollectionMetaToken
-//	 * @expectedException InvalidArgumentException
-//	 */
-//    public function testBuildCollection_badUrl() {
-//    	$this->site->buildLiveCommentsCollection("title", "articleId", "url");
-//    }
-//
-//	/**
-//	 * @covers Livefyre\Core\Collection::buildCollectionMetaToken
-//	 * @expectedException InvalidArgumentException
-//	 */
-//    public function testBuildCollection_badTitle() {
-//        $this->site->buildLiveCommentsCollection("1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456", "articleId", "http://www.url.com");
-//    }
-//
-//    /**
-//     * @covers Livefyre\Core\Collection::buildCollectionMetaToken
-//     * @expectedException InvalidArgumentException
-//     */
-//    public function testBuildCollection_badType() {
-//        $this->site->buildCollection("bad type", "title", "articleId", "http://livefyre.com");
-//    }
-//
-//    public function testBuildCollectionMetaToken() {
-//        $collection = $this->site->buildLiveCommentsCollection("title", "articleId", "https://www.url.com");
-//        $this->assertNotNull($collection->buildCollectionMetaToken());
-//
-//        $collection->getData()->setTags("tags");
-//
-//        $token = $collection->buildCollectionMetaToken();
-//        $decoded = JWT::decode($token, $this->config->SITE_KEY);
-//
-//        $this->assertEquals("tags", $decoded->{"tags"});
-//    }
-//
-//    public function testBuildChecksum() {
-//        $collection = $this->site->buildLiveCommentsCollection("title", "articleId", "http://livefyre.com");
-//        $collection->getData()->setTags("tags");
-//
-//        $checksum = $collection->buildChecksum();
-//
-//        $this->assertEquals("8bcfca7fb2187b1dcb627506deceee32", $checksum);
-//    }
-//
-//    public function testNetworkIssued() {
-//        $collection = $this->site->buildLiveCommentsCollection("title", "articleId", "http://livefyre.com");
-//        $this->assertFalse($collection->isNetworkIssued());
-//
-//        $collection->getData()->setTopics(array(Topic::create($this->site, "ID", "LABEL")));
-//        $this->assertFalse($collection->isNetworkIssued());
-//
-//        $collection->getData()->setTopics(array(Topic::create($this->site->getNetwork(), "ID", "LABEL")));
-//        $this->assertTrue($collection->isNetworkIssued());
-//
-//    }
-//}
+
+namespace Livefyre;
+
+
+use Livefyre\Dto\Topic;
+use Livefyre\Utils\JWT;
+
+class CollectionTest extends \PHPUnit_Framework_TestCase {
+    private $config;
+    private $site;
+
+    protected function setUp() {
+        $this->config = new LfTest();
+        $this->config->setPropValues("prod");
+        $this->site = Livefyre::getNetwork($this->config->NETWORK_NAME, $this->config->NETWORK_KEY)->getSite($this->config->SITE_ID, $this->config->SITE_KEY);
+    }
+
+    public function testApi() {
+        $name = "PHPCreateCollection" . time();
+
+        $collection = $this->site->buildLiveCommentsCollection($name, $name, "http://answers.livefyre.com/PHP");
+        $collection->createOrUpdate();
+
+        $id = $collection->getCollectionContent()->{"collectionSettings"}->{"collectionId"};
+        $this->assertEquals($id, $collection->getData()->getId());
+    }
+
+	/**
+	 * @covers Livefyre\Core\Collection::buildCollectionMetaToken
+	 * @expectedException InvalidArgumentException
+	 */
+    public function testBuildCollection_badUrl() {
+    	$this->site->buildLiveCommentsCollection("title", "articleId", "url");
+    }
+
+	/**
+	 * @covers Livefyre\Core\Collection::buildCollectionMetaToken
+	 * @expectedException InvalidArgumentException
+	 */
+    public function testBuildCollection_badTitle() {
+        $this->site->buildLiveCommentsCollection("1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456", "articleId", "http://www.url.com");
+    }
+
+    /**
+     * @covers Livefyre\Core\Collection::buildCollectionMetaToken
+     * @expectedException InvalidArgumentException
+     */
+    public function testBuildCollection_badType() {
+        $this->site->buildCollection("bad type", "title", "articleId", "http://livefyre.com");
+    }
+
+    public function testBuildCollectionMetaToken() {
+        $collection = $this->site->buildLiveCommentsCollection("title", "articleId", "https://www.url.com");
+        $this->assertNotNull($collection->buildCollectionMetaToken());
+
+        $collection->getData()->setTags("tags");
+
+        $token = $collection->buildCollectionMetaToken();
+        $decoded = JWT::decode($token, $this->config->SITE_KEY);
+
+        $this->assertEquals("tags", $decoded->{"tags"});
+    }
+
+    public function testBuildChecksum() {
+        $collection = $this->site->buildLiveCommentsCollection("title", "articleId", "http://livefyre.com");
+        $collection->getData()->setTags("tags");
+
+        $checksum = $collection->buildChecksum();
+
+        $this->assertEquals("8bcfca7fb2187b1dcb627506deceee32", $checksum);
+    }
+
+    public function testNetworkIssued() {
+        $collection = $this->site->buildLiveCommentsCollection("title", "articleId", "http://livefyre.com");
+        $this->assertFalse($collection->isNetworkIssued());
+
+        $collection->getData()->setTopics(array(Topic::create($this->site, "ID", "LABEL")));
+        $this->assertFalse($collection->isNetworkIssued());
+
+        $collection->getData()->setTopics(array(Topic::create($this->site->getNetwork(), "ID", "LABEL")));
+        $this->assertTrue($collection->isNetworkIssued());
+
+    }
+}
