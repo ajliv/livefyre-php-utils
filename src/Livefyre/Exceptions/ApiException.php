3426e910772fc921f1b734ddf1239db74e37f425
David Lee <dlee@livefyre.com>
2015-03-30 19:30:08 -0700

    fixing for older version of php

--- src/Livefyre/Exceptions/ApiException.php
+++ src/Livefyre/Exceptions/ApiException.php
@@ -7,11 +7,12 @@ namespace Livefyre\Exceptions;
  */
 class ApiException extends LivefyreException {
 
-    private $apiStatus = array(
+    private static $apiStatus = array(
         "c400" => "Please check the contents of your request. Error code 400.",
         "c401" => "The request requires authentication via an HTTP Authorization header. Error code 401.",
         "c403" => "The server understood the request; but is refusing to fulfill it. Error code 403.",
         "c404" => "The requested resource was not found. Error code 404.",
+        "c409" => "Specific to createOrUpdate. Filler message",
         "c500" => "Livefyre appears to be down. Please see status.livefyre.com or contact us for more information. Error code 500.",
         "c501" => "The requested functionality is not currently supported. Error code 501.",
         "c502" => "The server; while acting as a gateway or proxy; received an invalid response from the upstream server it accessed in attempting to fulfill the request at this time. Error code 502.",
@@ -19,7 +20,7 @@ class ApiException extends LivefyreException {
     );
 
     public function __construct($statusCode) {
-        parent::__construct($this->apiStatus["c".$statusCode], $statusCode);
+        parent::__construct(self::$apiStatus["c".$statusCode], $statusCode);
     }
 
     public function __toString() {
